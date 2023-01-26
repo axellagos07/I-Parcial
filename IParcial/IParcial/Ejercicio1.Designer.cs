99ff47c8c493bd53809ea3032e3e96ea6150553d
axellagos07 <123529981+axellagos07@users.noreply.github.com>
2023-01-25 22:43:06 -0600

    Segundo commint
    Segundo commit del repositorio(Realiza la operación de suma)

--- IParcial/IParcial/Ejercicio1.Designer.cs
+++ IParcial/IParcial/Ejercicio1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IParcial
 {
-    partial class Form1
+    partial class Ejercicio1
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.Numero2TextBox = new System.Windows.Forms.TextBox();
+            this.SumarButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Resultadolabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 2:";
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Location = new System.Drawing.Point(311, 58);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.Size = new System.Drawing.Size(122, 20);
+            this.Numero1TextBox.TabIndex = 2;
+            // 
+            // Numero2TextBox
+            // 
+            this.Numero2TextBox.Location = new System.Drawing.Point(311, 86);
+            this.Numero2TextBox.Name = "Numero2TextBox";
+            this.Numero2TextBox.Size = new System.Drawing.Size(122, 20);
+            this.Numero2TextBox.TabIndex = 3;
+            this.Numero2TextBox.TextChanged += new System.EventHandler(this.Numero2TextBox_TextChanged);
+            // 
+            // SumarButton
+            // 
+            this.SumarButton.Location = new System.Drawing.Point(311, 125);
+            this.SumarButton.Name = "SumarButton";
+            this.SumarButton.Size = new System.Drawing.Size(113, 62);
+            this.SumarButton.TabIndex = 4;
+            this.SumarButton.Text = "Sumar";
+            this.SumarButton.UseVisualStyleBackColor = true;
+            this.SumarButton.Click += new System.EventHandler(this.SumarButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Resultadolabel
+            // 
+            this.Resultadolabel.AutoSize = true;
+            this.Resultadolabel.Location = new System.Drawing.Point(449, 125);
+            this.Resultadolabel.Name = "Resultadolabel";
+            this.Resultadolabel.Size = new System.Drawing.Size(35, 13);
+            this.Resultadolabel.TabIndex = 6;
+            this.Resultadolabel.Text = "label4";
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(669, 377);
+            this.Controls.Add(this.Resultadolabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SumarButton);
+            this.Controls.Add(this.Numero2TextBox);
+            this.Controls.Add(this.Numero1TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Ejercicio1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.TextBox Numero2TextBox;
+        private System.Windows.Forms.Button SumarButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Resultadolabel;
     }
 }
 
--- IParcial/IParcial/Ejercicio1.cs
+++ IParcial/IParcial/Ejercicio1.cs
@@ -10,11 +10,46 @@ using System.Windows.Forms;
 
 namespace IParcial
 {
-    public partial class Form1 : Form
+    public partial class Ejercicio1 : Form
     {
-        public Form1()
+        public Ejercicio1()
         {
             InitializeComponent();
+        }
+
+        private void Ejercicio1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Numero2TextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SumarButton_Click(object sender, EventArgs e)
+        {
+            //Variables 
+
+            decimal numero1 = Convert.ToDecimal(Numero1TextBox.Text);
+            decimal numero2 = Convert.ToDecimal(Numero2TextBox.Text);
+
+            decimal resultado = numero1 + numero2;
+
+            //MessageBox.Show("La suma es:" + resultado);
+
+            Resultadolabel.Text = Convert.ToString(resultado);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
